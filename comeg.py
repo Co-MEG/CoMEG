b3ff57811e24fac8713600cead0cc9c191735728
simondelarue <sim.delarue@gmail.com>
2022-08-05 09:58:55 +0200

    added roc auc metric

--- comeg.py
+++ comeg.py
@@ -7,16 +7,32 @@ import numpy as np
 import json
 import os
 
+from sklearn import metrics
+
+import matplotlib.pyplot as plt
+
 # Data path
 IN_PATH = 'goodreads_poetry'
 USE_CACHE = True
-TEST_SIZE = 0.3
+TEST_SIZE = 0.1
+TRAIN_TEST_SPLIT = False
+PATH_RES = os.path.join(os.getcwd(), 'data', 'goodreads_poetry', 'result', 'adamic_adar.json')
+
+
+def plot_roc_auc(y_true, y_pred, ax):
+    fpr, tpr, thresholds = metrics.roc_curve(y_true,  y_pred) 
+    auc = metrics.roc_auc_score(y_true, y_pred)
+
+    ax.plot(fpr, tpr, label='Adamic Adar')
+    ax.set_title(f'ROC AUC Curve - AUC = {auc:.4f}', weight='bold')
+    ax.legend()
 
 if __name__ == '__main__':
     
     # Build graph
     g = BipartiteGraph()
     g.load_data(IN_PATH, use_cache=USE_CACHE)
+    m = g.number_of_edges()
 
     # Verif
     print(type(g))
@@ -24,53 +40,50 @@ if __name__ == '__main__':
     print(f"Number of books: {len(g.V.get('right'))}")
     print(f"Number of reviews: {len(g.E)}")
 
+    
     # Link prediction
     # ----------------------
-    m = g.number_of_edges()
-
-    TRAIN_TEST_SPLIT = False
     if TRAIN_TEST_SPLIT:
         print('Splitting graph into train-test...')
         train_g, test_g = g.train_test_split(test_size=TEST_SIZE)
-    
-    """print(f"Number of users in train-test: {len(train_g.V.get('left'))}-{len(test_g.V.get('left'))}")
-    print(f"Number of books in train-test: {len(train_g.V.get('right'))}-{len(test_g.V.get('right'))}")
-    print(f"Number of reviews in train-test: {len(train_g.E)}-{len(test_g.E)}")
-    print(f"Number of edge attributes in train-test: {len(train_g.edge_attr)}-{len(test_g.edge_attr)}")
-    """
+        print(f'Number of reviews in test: {len(test_g.E)}')
+        print(f'Number of left nodes used: {len(set([e[0] for e in test_g.E]))}')
+        print(f'Number of right nodes used: {len(set([e[1] for e in test_g.E]))}')
+
     # Adamic Adar
-    first_node = g.V['left'][0]
-    if TRAIN_TEST_SPLIT:
-        neighbors = train_g.get_neighbors(first_node)
-    else:
-        neighbors = g.get_neighbors(first_node)
-    #neighbors_2hops = g.get_neighbors_2hops(first_node)
-    print(f'# Neighbors of node: {first_node} : {len(neighbors)}')
-    #print(f'# Neighbors of node from same set: {first_node} : {len(neighbors_2hops)}')
-    
+    """
     print('Adamic Adar index...')
     aa = AdamicAdar()
-    n_nodes_predict = 5
+    n_nodes_predict = 100
+    first_node = g.V['left'][0]
+    
     if TRAIN_TEST_SPLIT:
+        neighbors = train_g.get_neighbors(first_node)
         scores = aa.predict_sample(train_g, train_g.V['left'][:n_nodes_predict])
     else:
+        neighbors = g.get_neighbors(first_node)
         scores = aa.predict_sample(g, g.V['left'][:n_nodes_predict])
     
-    # Save results for index
-    path = os.path.join(os.getcwd(), 'data', 'goodreads_poetry', 'result', 'adamic_adar.json')
-    with open(path, "w") as f:
+    # Save results for AA index
+    with open(PATH_RES, "w") as f:
         json.dump(scores , f) 
+    """
+    
+    # Evaluation
+    # ----------
 
+    results = json.load(open(PATH_RES))
 
-    # Test first order metrics
-        # select randomly x% of pairs of nodes in the original graph -> computationaly possible
-        # select randomly y% of edges in the test set.
-        # select randomly z% of edges in the train set.
-        # compute index on these pairs
-        # test with ground truth, i.e. compare score with the existence/absence of edge in the orignial graph
-        # Compute ROC AUC curve
+    # ground truth label
+    y_pred, y_true = zip(*[(values[2], g.has_edge(values[0], values[1])) for _, values in results.items()])
+    print(f'Number of predicted edges: {len(y_pred)}')
 
+    # Plot results
+    fig, ax = plt.subplots(1, 1, figsize=(12, 7))
+    plot_roc_auc(y_true, y_pred, ax)
+    plt.show()
 
+    
     #idx = np.where(train_g.names_row == first_node)[0][0]
     #scores = aa.predict(idx)
     #scores = aa.predict(np.arange(0, 377799))
@@ -81,7 +94,6 @@ if __name__ == '__main__':
     #scores = aa.predict(g, transpose=False)
     #print(scores[:10])
     
-
     # SKNETWORK - VERIF
     """total = 0
     idx = np.where(g.names_row == first_node)[0][0]
