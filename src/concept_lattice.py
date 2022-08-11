e2d69777bcca41bf2be6f112548c4d0618a111df
simondelarue <sim.delarue@gmail.com>
2022-08-11 10:45:04 +0200

    cleaned CbO algo + docstring

--- src/concept_lattice.py
+++ src/concept_lattice.py
@@ -54,65 +54,55 @@ def close_by_one(context: FormalContext) -> List[FormalConcept]:
     """
     # Find all concepts using Close by One (CbO) algorithm
     L = []
-    for i, g in enumerate(context.G):
+    for g in context.G:
         process([g], context.intention([g]), L, context, g)
     return L
 
-def process(A, attrs, L, context, g):
-    print(f"**Process({A}, {attrs})")
+def process(A: list, attrs: list, L: list, context: FormalContext, current_obj: int):
+    """Process Close by One algorithm.
 
-    #D = attrs
+    Parameters
+    ----------
+    A : list
+        List of objects
+    attrs : list
+        List of attributes
+    L : list
+        List of valid concepts
+    context : FormalContext
+        Formal Context on which CbO algorithm is applied
+    current_obj : int
+        Current selected object
+    """    
+
+    # (C, D) forms a formal concept
     D = context.intention(A)
     C = context.extension(D, return_names=False)
-    print(f"  C (extent): {context.G[C]}")
-    print(f"  D (intent): {D}")
 
-    A_i = [context.G2idx.get(a) for a in A]
+    A_i = [context.G2idx.get(a) for a in A] # object indexes
     
     # If a concept has already been elaborated, the extent C contains at least one object lexicographically smaller 
-    # than the current object -> we verify that C < A_i is False
-    #print(f'  Test if {context.G[C]} is lex less than {context.G[A_i]} : {minus_lex(set(C), set(A_i))}')
-    #print(f'  Test if {set} is lex less than {context.extension(attrs, return_names=False)}')
-    
-    # new
-    #diff = set(A_i).difference(set(C))
-    #is_lex_smaller = any([h < np.max(context.extension(attrs, return_names=False)) for h in diff])
-    #if not is_lex_smaller:
-
-    # newnew
+    # than the current object -> we verify that (C - A) < current_object is False
     diff = set(C).difference(set(A_i))
-    is_lex_smaller = any([h < np.max(context.G2idx.get(g)) for h in diff])
-    print(f'  Test if {diff} (i.e {C}-{A_i}) is lex less than {np.max(context.G2idx.get(g))}')
+    is_lex_smaller = any([h < np.max(context.G2idx.get(current_obj)) for h in diff])
+    
     if not is_lex_smaller:
-    #if not minus_lex(set(C), set(A_i)):
-        print(f'  New extent: {context.G[C]}')
 
+        # Add concept to result
         L.append((list(context.G[C]), D))
-        print(f'  Added concept: {(context.G[C], D)}')
 
-        # Complementary objects of current object, which are lexicographically above the current object
+        # Candidates: complementary objects of current object
         comp_g = set(np.arange(0, len(context.G))).difference(set(A_i))
-        #candidates = [h for h in comp_g if minus_lex(set(A_i), set([h]))]
-        
-        # new
-        #candidates = [h for h in comp_g if h > np.max(context.extension(attrs, return_names=False))]
         candidates = [h for h in comp_g ]
-        print(f'  Try to add some more candidates in the extent...')
+        
         for f in candidates:
-            print(f'    Candidate: {context.G[f]}')
             Z = set(C).union(set([f])) # Add candidate object to extension
             Z_labels = context.G[list(Z)]
-            print(f'    Z: {Z_labels}')
             f_intention = context.intention(context.G[[f]])
             Y = set(D).intersection(f_intention) # Compute new intention
-            print(f'    Y: {Y}')
             if len(Y) != 0:
                 X = context.extension(Y)
-                print(f'    X: {X}')
-                process(Z_labels, f_intention, L, context, g)
-
-    #print(f'  L : {(L)}')
-    print()
+                process(Z_labels, f_intention, L, context, current_obj)
 
 def minus_lex(A: set, B: set) -> bool:
     diff = sorted(A.difference(B))
@@ -120,8 +110,6 @@ def minus_lex(A: set, B: set) -> bool:
     #diff = sorted(B.difference(A))
     #return len(diff) > 0 and \
     #    any([A.intersection(set(np.arange(1, i))) == B.intersection(set(np.arange(1, i))) for i in diff])
-
-
 
 def get_algo(algo: str) -> object:
     """Return algorithm function according to algorithm name.
