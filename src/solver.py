9cc8cb95db488f512b45c19e517618428e53f8ab
simondelarue <sim.delarue@gmail.com>
2022-08-29 11:37:01 +0200

    optimization on graph size

--- src/solver.py
+++ src/solver.py
@@ -1,14 +1,39 @@
+from typing import TYPE_CHECKING
 import numpy as np
 from pulp import *
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from src.concept_lattice import ConceptLattice
+
 
 class Solver():
-    def __init__(self, concepts: list):
-        self.concepts = np.array(concepts, dtype='object')
+    def __init__(self, lattice: 'ConceptLattice'):
+        self.lattice = lattice
+        self.concepts = np.array(lattice.concepts, dtype='object')
         self.concepts_idx = np.arange(0, len(self.concepts))
         self.x = pulp.LpVariable.dicts("x", 
                                 indices = self.concepts_idx, 
                                 lowBound=0, upBound=1, 
                                 cat='Integer', indexStart=[])
+    
+    def init_model_variables(self):
+        ext_len, int_len = [], []
+        for c in self.concepts:
+            
+            # Size of intent
+            int_len.append(len(c[1]))
+
+            # Size of extent
+            #ext_len.append(len(c[0]))
+
+            # Size of graph induced by concept
+            attr_idxs = [self.lattice.context.G2idx.get(i) for i in c[0]]
+            g_concept = self.lattice.context.graph.adjacency_csr.T[attr_idxs]
+            n_right, n_left = g_concept.shape
+            m = g_concept.nnz
+            ext_len.append(n_right + n_left + m)
+
+        return ext_len, int_len
     
     def model(self, k: int = 5):
         """Build optmization model with objective and constraints.
@@ -23,10 +48,7 @@ class Solver():
         ``pulp`` problem object.
         """        
         # Variables are lengths of extent and intent of concepts
-        ext_len, int_len = [], []
-        for c in self.concepts:
-            ext_len.append(len(c[0]))
-            int_len.append(len(c[1]))
+        ext_len, int_len = self.init_model_variables()
 
         # Find subset of 5 concepts that maximize both lengths of extents and intents
         prob = pulp.LpProblem("Best_concepts", LpMaximize)
