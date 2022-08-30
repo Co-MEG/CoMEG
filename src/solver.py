0d1770da9db25d617c5c2765dbb9477edee996d9
simondelarue <sim.delarue@gmail.com>
2022-08-30 11:01:42 +0200

    updated constraints of LP problem

--- src/solver.py
+++ src/solver.py
@@ -20,10 +20,13 @@ class Solver():
     
     def init_model_variables(self):
         ext_len, int_len = [], []
+        n_unique_attr = len(np.unique([x.split('_')[:-1] for x in self.lattice.context.M]))
+        
         for c in self.concepts:
             
             # Size of intent
-            int_len.append(len(c[1]) / len(self.lattice.context.M))
+            #int_len.append(len(c[1]) / len(self.lattice.context.M))
+            int_len.append(len(c[1]) / n_unique_attr)
 
             # Size of extent
             #ext_len.append(len(c[0]))
@@ -36,9 +39,9 @@ class Solver():
             ext_len.append((n_right + n_left + m) / self.lattice.context.graph.size())
 
             print(round((n_right + n_left + m) / self.lattice.context.graph.size(), 3),
-                round(len(c[1]) / len(self.lattice.context.M), 3),
+                round(len(c[1]) / n_unique_attr, 3),
                 c)
-
+        print(f'Number of unique attributes: {n_unique_attr}')
         return ext_len, int_len
     
     def multi_obj_model(self, k: int = 5):
@@ -51,35 +54,48 @@ class Solver():
         probs = []
         objs = []
         min_obj = np.inf
+        max_obj = 0
         for alpha in np.arange(0, 1 + step_size, step_size):
             prob = pulp.LpProblem("Best_concepts Multi objectives", LpMaximize)
-            #prob += pulp.lpSum([self.x[c] * (alpha * ext_len[c] + (1 - alpha) * int_len[c]) for c in self.concepts_idx])
-            prob += alpha * pulp.lpSum([self.x[c] * ext_len[c] for c in self.concepts_idx]) \
-                + (1 - alpha) * pulp.lpSum([self.x[c] * int_len[c] * 3 for c in self.concepts_idx])
-            prob += pulp.lpSum([self.x[c] for c in self.concepts_idx]) == k
             
+            prob += alpha * pulp.lpSum([self.x[c] * ext_len[c] for c in self.concepts_idx]) \
+                + (1 - alpha) * pulp.lpSum([self.x[c] * int_len[c] for c in self.concepts_idx])
+            prob += pulp.lpSum([self.x[c] for c in self.concepts_idx]) == k
+            for c in self.concepts_idx:
+                prob += self.x[c] * ext_len[c] <= 0.95
+                prob += self.x[c] * int_len[c] <= 0.95
+            
+
             solution = prob.solve(PULP_CBC_CMD(msg=False))
             solutionTable.loc[int(alpha*1/step_size)] = [alpha, pulp.value(prob.objective)]
-            if pulp.value(prob.objective) <= min_obj:
+            
+            #if pulp.value(prob.objective) >= max_obj:
+            if pulp.value(prob.objective) <= min_obj:    
+            
                 min_obj = pulp.value(prob.objective)
                 min_prob = prob
                 min_alpha = alpha
+                """max_obj = pulp.value(prob.objective)
+                max_prob = prob
+                max_alpha = alpha"""
         
         fig, ax = plt.subplots(1, 1, figsize=(12, 7))
         print(solutionTable)
         plt.plot(solutionTable['alpha'], solutionTable['obj_value'], color='g')
         plt.xlabel('alpha')
         plt.ylabel('Objective value')
-        #plt.show()
-        PATH_RES = os.path.join(os.getcwd(), 'data', 'goodreads_poetry', 'result')
-        res = os.path.join(PATH_RES, 'img', f'LO_pareto_bc1d727746e210f315138932e0aacb11_13637887.eps')
-        plt.tight_layout()
-        plt.savefig(res)
+        plt.show()
+        # Save result img
+        #PATH_RES = os.path.join(os.getcwd(), 'data', 'goodreads_poetry', 'result')
+        #res = os.path.join(PATH_RES, 'img', f'LO_pareto_bc1d727746e210f315138932e0aacb11_13637887.eps')
+        #plt.tight_layout()
+        #lt.savefig(res)
 
         print(f'Alpha selected: {min_alpha}')
-
+        #print(f'Alpha selected: {max_alpha}')
+        #return max_prob
         return min_prob
-
+        
     def model(self, k: int = 5):
         """Build optmization model with objective and constraints.
 
@@ -125,7 +141,7 @@ class Solver():
         """prob = self.model(k=k)
         prob.solve(solver(msg=msg))"""
         
-        concept_idxs = np.array([int(v.name.split('_')[1]) for v in prob.variables() if v.varValue == 1])
+        concept_idxs = np.array([int(v.name.split('_')[1]) for v in prob.variables() if v.varValue > 0])
         scores = np.array([v.dj for v in prob.variables()])
 
         return list(self.concepts[concept_idxs]), scores
