2e1858aa7f3afd5ea2e107afe1798d8de3fea9ee
simondelarue <sim.delarue@gmail.com>
2023-02-21 15:24:28 +0100

    datasets verifications

--- evaluation.py
+++ evaluation.py
@@ -4,7 +4,7 @@ import pickle
 
 from baselines import get_louvain, get_community_graph, get_gnn, get_spectral, get_doc2vec
 from data import load_data, preprocess_data, load_patterns, get_pw_distance_matrix
-from metrics import information
+from metrics import information, information_summaries
 from summarization import get_summarized_graph, get_summarized_biadjacency, get_pattern_summaries
 from utils import pattern_attributes
 
@@ -13,13 +13,18 @@ from utils import pattern_attributes
 # Parameters
 # =================================================================
 
-datasets = ['wikivitals', 'wikivitals-fr', 'wikischools']
+#datasets = ['sanFranciscoCrimes']
+#datasets = ['wikivitals', 'wikivitals-fr', 'wikischools']
+#datasets = ['london']
+datasets = ['ingredients']
 #datasets = ['lastfm']
-betas = [8, 7, 6, 5]
+betas = [1]
 ss = [8, 7, 6, 5]
+inpath = '/Users/simondelarue/Documents/PhD/Research/Co-Meg/CoMEG/output/result/with_prob'
 
 with_order = [True]
-delta = 0.2
+with_prob = True
+gamma = 0.5
 informations = defaultdict()
 
 # =================================================================
@@ -41,7 +46,7 @@ for d, dataset in enumerate(datasets):
             new_biadjacency, words = preprocess_data(biadjacency, names_col, s)
             
             # Load patterns
-            patterns = load_patterns(dataset, b, s, True)
+            patterns = load_patterns(dataset, b, s, with_order[0], inpath, with_prob)
             
             # Summarized graph + features
             summarized_adj = get_summarized_graph(adjacency, patterns)
@@ -50,16 +55,21 @@ for d, dataset in enumerate(datasets):
             # Pattern summaries 
             pattern_summary_labels, mask = get_pattern_summaries(summarized_adj)
             n_p_summaries = len(np.unique(pattern_summary_labels))
+            print(f'# pattern summaries: {n_p_summaries}')
+            pattern_summarized_attributes = pattern_attributes(summarized_biadj, pattern_summary_labels, mask)
             
             # Louvain
-            louvain_labels = get_louvain(dataset, adjacency, n_p_summaries)
-            pattern_louvain_attributes = pattern_attributes(biadjacency, louvain_labels)
-            louvain_adj = get_community_graph(adjacency, louvain_labels)
+            if dataset != 'ingredients':
+                louvain_labels = get_louvain(dataset, adjacency, n_p_summaries)
+                pattern_louvain_attributes = pattern_attributes(biadjacency, louvain_labels)
+                louvain_adj = get_community_graph(adjacency, louvain_labels)
+                n_p_louvain = len(np.unique(louvain_labels))
 
             # GNN
-            gnn_labels = get_gnn(adjacency, biadjacency, labels, 16, n_p_summaries)
-            pattern_gnn_attributes = pattern_attributes(biadjacency, gnn_labels)
-            gnn_adj = get_community_graph(adjacency, gnn_labels)
+            if dataset != 'sanFranciscoCrimes' and dataset != 'london' and dataset != 'ingredients':
+                gnn_labels = get_gnn(adjacency, biadjacency, labels, 16, n_p_summaries)
+                pattern_gnn_attributes = pattern_attributes(biadjacency, gnn_labels)
+                gnn_adj = get_community_graph(adjacency, gnn_labels)
 
             # Spectral
             spectral_labels = get_spectral(adjacency, n_p_summaries)
@@ -67,37 +77,49 @@ for d, dataset in enumerate(datasets):
             spectral_adj = get_community_graph(adjacency, spectral_labels)
 
             # Doc2Vec
-            d2v_labels = get_doc2vec(biadjacency, names_col, n_p_summaries)
+            d2v_labels = get_doc2vec(biadjacency, dataset, names_col, n_p_summaries)
             pattern_d2v_attributes = pattern_attributes(biadjacency, d2v_labels)
             d2v_adj = get_community_graph(adjacency, d2v_labels)
             
             # Pariwise distances 
-            pw_distances_summaries = get_pw_distance_matrix(dataset, b, s, method='summaries')
-            pw_distances_louvain = get_pw_distance_matrix(dataset, b, s, method='louvain')
-            pw_distances_gnn = get_pw_distance_matrix(dataset, b, s, method='gnn_kmeans')
-            pw_distances_spectral = get_pw_distance_matrix(dataset, b, s, method='spectral_kmeans')
-            pw_distances_d2v = get_pw_distance_matrix(dataset, b, s, method='d2v_kmeans')
+            pw_distances_summaries = get_pw_distance_matrix(dataset, b, s, inpath + '/new', method='summaries')
+            if dataset != 'ingredients':
+                pw_distances_louvain = get_pw_distance_matrix(dataset, b, s, inpath + '/new', method='louvain')
+            if dataset != 'sanFranciscoCrimes' and dataset != 'london' and dataset != 'ingredients':
+                pw_distances_gnn = get_pw_distance_matrix(dataset, b, s, inpath + '/new', method='gnn_kmeans')
+            pw_distances_spectral = get_pw_distance_matrix(dataset, b, s, inpath + '/new', method='spectral_kmeans')
+            pw_distances_d2v = get_pw_distance_matrix(dataset, b, s, inpath + '/new', method='d2v_kmeans')
             
             # SG information
             print(f'    Summaries')
-            information_summaries = information(summarized_adj, summarized_biadj, pw_distances_summaries)
-            print(f'    Louvain')
-            information_louvain = information(louvain_adj, pattern_louvain_attributes, pw_distances_louvain)
-            print(f'    GNN')
-            information_gnn = information(gnn_adj, pattern_gnn_attributes, pw_distances_gnn)
+            information_p_summaries = information_summaries(adjacency, biadjacency, summarized_adj, pattern_summary_labels, n_p_summaries, pattern_summarized_attributes, pw_distances_summaries, dataset, b, s, gamma, 'summaries', inpath + '/new')
+            #information_summaries = information(summarized_adj, summarized_biadj, pw_distances_summaries)
+            if dataset != 'ingredients':
+                print(f'    Louvain')
+                information_louvain = information_summaries(adjacency, biadjacency, louvain_adj, louvain_labels, n_p_louvain, pattern_louvain_attributes, pw_distances_louvain, dataset, b, s, gamma, 'louvain', inpath + '/new')
+                #information_louvain = information(louvain_adj, pattern_louvain_attributes, pw_distances_louvain)
+            
+            if dataset != 'sanFranciscoCrimes' and dataset != 'london' and dataset != 'ingredients':
+                print(f'    GNN')
+                information_gnn = information_summaries(adjacency, biadjacency, gnn_adj, gnn_labels, n_p_summaries, pattern_gnn_attributes, pw_distances_gnn, dataset, b, s, gamma, 'gnn', inpath + '/new')
+            #information_gnn = information(gnn_adj, pattern_gnn_attributes, pw_distances_gnn)
             print(f'    Spectral')
-            information_spectral = information(spectral_adj, pattern_spectral_attributes, pw_distances_spectral)
+            information_spectral = information_summaries(adjacency, biadjacency, spectral_adj, spectral_labels, n_p_summaries, pattern_spectral_attributes, pw_distances_spectral, dataset, b, s, gamma, 'spectral', inpath + '/new')
+            #information_spectral = information(spectral_adj, pattern_spectral_attributes, pw_distances_spectral)
             print(f'    Doc2Vec')
-            information_d2v = information(d2v_adj, pattern_d2v_attributes, pw_distances_d2v)
+            information_d2v = information_summaries(adjacency, biadjacency, d2v_adj, d2v_labels, n_p_summaries, pattern_d2v_attributes, pw_distances_d2v, dataset, b, s, gamma, 'd2v', inpath + '/new')
+            #information_d2v = information(d2v_adj, pattern_d2v_attributes, pw_distances_d2v)
             
             # Save information in dict
-            informations[dataset][b]['summaries'].append(information_summaries)
-            informations[dataset][b]['louvain'].append(information_louvain)
-            informations[dataset][b]['gnn'].append(information_gnn)
+            informations[dataset][b]['summaries'].append(information_p_summaries)
+            if dataset != 'ingredients':
+                informations[dataset][b]['louvain'].append(information_louvain)
+            if dataset != 'sanFranciscoCrimes' and dataset != 'london' and dataset != 'ingredients':
+                informations[dataset][b]['gnn'].append(information_gnn)
             informations[dataset][b]['spectral'].append(information_spectral)
             informations[dataset][b]['doc2vec'].append(information_d2v)
 
 # Save result
-with open(f'informations_evaluation_new.pkl', 'wb') as f:
+with open(f'{inpath}/new/informations_evaluation_new_conc.pkl', 'wb') as f:
     pickle.dump(informations, f)
     
--- redundancy_statistics.py
+++ redundancy_statistics.py
@@ -9,6 +9,7 @@
 # **************************************************************************
 
 from collections import defaultdict
+import os
 import gensim
 import numpy as np
 import pickle
@@ -19,12 +20,13 @@ from sknetwork.data import load_netset
 from sknetwork.gnn import GNNClassifier
 from sknetwork.topology import get_connected_components
 from sknetwork.utils import get_degrees, KMeansDense
+from baselines import get_doc2vec
 
 from corpus import MyCorpus
 from data import load_data
 from distances import pairwise_wd_distance
-from summarization import get_summarized_graph
-from utils import build_pattern_attributes
+from summarization import get_pattern_summaries, get_summarized_biadjacency, get_summarized_graph
+from utils import build_pattern_attributes, load_gensim_model, save_gensim_model
 
 
 def load_result(path, filename):
@@ -32,12 +34,18 @@ def load_result(path, filename):
         result = pickle.load(data)
     return result
 
-datasets = ['wikivitals', 'wikivitals-fr', 'wikischools']
+#datasets = ['ingredients']
+#datasets = ['london']
+#datasets = ['wikivitals', 'wikivitals-fr', 'wikischools']
+datasets = ['wikivitals']
+#datasets = ['sanFranciscoCrimes']
+#datasets = ['ingredientsGraph']
 #datasets = ['lastfm']
 sorted_attributes = True
 with_prob = True
-ss = [8, 7, 6, 5]
-betas = [8, 7, 6, 5]
+ss = [5]
+betas = [5]
+delta = 0
 #ss = [1]
 #betas = [] 
 avgs = defaultdict(dict)
@@ -45,10 +53,12 @@ if with_prob:
     outpath = 'output/result/with_prob'
 else:
     outpath = 'output/result'
-resolutions = {'wikivitals-fr': {1: 0, 3: 0, 5: 0.3, 6: 0.4, 8: 0.6, 10: 0.8, 12: 0.9, 14: 1.05, 15: 1.1, 20: 1.4, 27: 1.89, 47: 3, 58: 3.87, 85: 5.58}, 
-                'wikivitals': {4: 0.5, 7: 0.8, 11: 1, 13: 1.3, 14: 1.18, 16: 1.5, 19: 1.6, 21: 1.76, 23: 1.83, 28: 2.3, 29: 2.4, 31: 2.5, 34: 2.7, 47: 3.903, 49: 3.95, 65: 4.70, 85: 5.58, 101: 6.4, 118: 7.25}, 
-                'wikischools': {3: 0.4, 4: 0.45, 8: 1, 3: 0.4, 8: 0.58, 9: 0.9, 10: 1.18, 13: 1.4, 15: 1.5, 18: 1.69, 22: 1.95, 32: 2.51, 37: 2.6, 39: 2.75, 40: 2.75,  46: 2.99, 57: 3.7, 58: 3.7, 70: 4.2, 73: 4.41, 79: 4.59},
-                'lastfm': {60: 2.74, 32: 0.65}}
+resolutions = {'wikivitals-fr': {1: 0, 3: 0, 5: 0.3, 6: 0.4, 8: 0.6, 9: 0.7, 10: 0.8, 11: 0.9, 12: 0.9, 14: 1.05, 15: 1.1, 20: 1.4, 26: 1.86, 27: 1.89, 47: 3, 53: 3.8, 58: 3.87, 85: 5.58}, 
+                'wikivitals': {4: 0.5, 7: 0.8, 11: 1, 13: 1.3, 14: 1.18, 16: 1.5, 18: 1.6, 19: 1.6, 21: 1.76, 23: 1.83, 28: 2.3, 29: 2.4, 31: 2.5, 34: 2.7, 40: 3.2, 47: 3.903, 49: 3.95, 57: 4.2, 65: 4.70, 85: 5.58, 101: 6.4, 109: 6.7, 118: 7.25}, 
+                'wikischools': {3: 0.4, 4: 0.45, 8: 1, 3: 0.4, 8: 0.58, 9: 0.9, 10: 1.18, 13: 1.4, 15: 1.5, 18: 1.69, 22: 1.95, 24: 2.1, 25: 2.2, 32: 2.51, 37: 2.6, 39: 2.75, 40: 2.75,  46: 2.99, 52: 3.3, 57: 3.7, 58: 3.7, 70: 4.2, 73: 4.41, 74: 4.45, 79: 4.59},
+                'lastfm': {60: 2.74, 32: 0.65},
+                'sanFranciscoCrimes': {5: 0.13, 6: 0.14},
+                'london': {1: 0.05}}
 
 # Run experiment
 # ------------------------------------------------------------------
@@ -64,7 +74,7 @@ for dataset in datasets:
             # Load result
             # ------------------------------------------------------------------
             if with_prob:
-                filename = f'result_{dataset}_{b}_{s}_order{str(sorted_attributes)}_prob'
+                filename = f'result_{dataset}_{b}_{s}_order{str(sorted_attributes)}_delta_{delta}'
             else:
                 filename = f'result_{dataset}_{b}_{s}_order{str(sorted_attributes)}'
             result = load_result(outpath, filename)
@@ -94,31 +104,21 @@ for dataset in datasets:
 
             # Graph summarization
             # ------------------------------------------------------------------
-            summarized_adjacency = get_summarized_graph(adjacency, result)
-
-            # Summarized graph filtered on used nodes
-            mask = np.flatnonzero(summarized_adjacency.dot(np.ones(summarized_adjacency.shape[1])))
-
-            # Summarized biadjacency
-            summarized_biadjacency = np.zeros((adjacency.shape[0], biadjacency.shape[1]))
-            for c in result:
-                if len(c[1]) > 0:
-                    for node in c[0]:
-                        summarized_biadjacency[node, c[1]] += 1
-            summarized_biadjacency = sparse.csr_matrix(summarized_biadjacency.astype(bool), shape=summarized_biadjacency.shape)
-            
-            # Number of connected components NOT considering isolated nodes
-            labels_cc_summarized = get_connected_components(summarized_adjacency[mask, :][:, mask])
-            nb_cc = len(np.unique(labels_cc_summarized)) 
-            print(nb_cc)
+            # Summarized adjacency and biadjacency
+            summarized_adjacency = get_summarized_graph(adjacency, result)            
+            summarized_biadjacency = get_summarized_biadjacency(adjacency, filt_biadjacency, result)
+            # Pattern summaries
+            labels_cc_summarized, mask = get_pattern_summaries(summarized_adjacency)
+            nb_cc = len(np.unique(labels_cc_summarized))
 
             # Baseline methods
             # ------------------------------------------------------------------
 
             # Louvain
-            louvain = Louvain(resolution=resolutions.get(dataset).get(nb_cc)) 
-            labels_louvain = louvain.fit_transform(adjacency)
-            nb_louvain = len(np.unique(labels_louvain))
+            if dataset != 'ingredients':
+                louvain = Louvain(resolution=resolutions.get(dataset).get(nb_cc)) 
+                labels_louvain = louvain.fit_transform(adjacency)
+                nb_louvain = len(np.unique(labels_louvain))
 
             # GNN
             features = biadjacency
@@ -141,11 +141,16 @@ for dataset in datasets:
             kmeans_spectral_labels = kmeans.fit_transform(adjacency)
             
             # Doc2Vec model on whole biadjacency matrix
-            corpus = list(MyCorpus(biadjacency, orig_words))
-            model = gensim.models.doc2vec.Doc2Vec(vector_size=15, min_count=5, epochs=300)
-            model.build_vocab(corpus)
-            # Training model
-            model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
+            if not os.path.exists(f'models/new_models/gensim_model_{dataset}.model'):
+                corpus = list(MyCorpus(biadjacency, orig_words))
+                model = gensim.models.doc2vec.Doc2Vec(vector_size=15, min_count=5, epochs=300)
+                model.build_vocab(corpus)
+                # Training model
+                model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
+                # Save model
+                save_gensim_model(model, 'models/new_models/', f'gensim_model_{dataset}')
+            else:
+                model = load_gensim_model('models/new_models/', f'gensim_model_{dataset}')
 
             # KMeans model on d2v embeddings
             kmeans = KMeansDense(n_clusters=nb_cc) # k = number of connected components in summarized graph
@@ -154,33 +159,45 @@ for dataset in datasets:
             # Concept x attributes matrices for each method
             # ------------------------------------------------------------------
             if len(labels) > 0:
-                concept_attributes, concept_summarized_attributes, concept_louvain_attributes, concept_gnn_kmeans_attributes, concept_spectral_kmeans_attributes, concept_doc2vec_kmeans_attributes = build_pattern_attributes(result, 
-                                                                                                    biadjacency, labels_cc_summarized, labels_louvain, kmeans_gnn_labels, kmeans_spectral_labels, kmeans_doc2vec_labels)
-            else:     
+                if dataset != 'ingredients':
+                    concept_attributes, concept_summarized_attributes, concept_louvain_attributes, concept_gnn_kmeans_attributes, concept_spectral_kmeans_attributes, concept_doc2vec_kmeans_attributes = build_pattern_attributes(result, 
+                                                                                                        biadjacency, labels_cc_summarized, labels_louvain, kmeans_gnn_labels, kmeans_spectral_labels, kmeans_doc2vec_labels)
+                else:
+                    # Louvain cannot be applied
+                    concept_attributes, concept_summarized_attributes, _, concept_gnn_kmeans_attributes, concept_spectral_kmeans_attributes, concept_doc2vec_kmeans_attributes = build_pattern_attributes(result, 
+                                                                                                        biadjacency, labels_cc_summarized, labels_cc_summarized, kmeans_gnn_labels, kmeans_spectral_labels, kmeans_doc2vec_labels)
+            elif dataset != 'ingredients':
+                # Louvain cannot be applied
                 concept_attributes, concept_summarized_attributes, concept_louvain_attributes, _, concept_spectral_kmeans_attributes, concept_doc2vec_kmeans_attributes = build_pattern_attributes(result, 
-                                                                                                    biadjacency, labels_cc_summarized, labels_louvain, labels_louvain, kmeans_spectral_labels, kmeans_doc2vec_labels)                                                                               
+                                                                                                    biadjacency, labels_cc_summarized, labels_louvain, labels_cc_summarized, kmeans_spectral_labels, kmeans_doc2vec_labels)        
+            else:
+                # GNN and Louvain cannot be applied
+                concept_attributes, concept_summarized_attributes, _, _, concept_spectral_kmeans_attributes, concept_doc2vec_kmeans_attributes = build_pattern_attributes(result, 
+                                                                                                    biadjacency, labels_cc_summarized, labels_cc_summarized, labels_cc_summarized, kmeans_spectral_labels, kmeans_doc2vec_labels)                                                                               
             
             # Pairwise Wassertein distances between embeddings
             # ------------------------------------------------------------------
             d2v_wd_matrix_concepts = pairwise_wd_distance(concept_attributes, nb_cc, model, sorted_names_col)
             d2v_wd_matrix_summarized = pairwise_wd_distance(concept_summarized_attributes, nb_cc, model, sorted_names_col)
-            d2v_wd_matrix_louvain = pairwise_wd_distance(concept_louvain_attributes, nb_louvain, model, words)
+            if dataset != 'ingredients':
+                d2v_wd_matrix_louvain = pairwise_wd_distance(concept_louvain_attributes, nb_louvain, model, words)
             if len(labels) > 0:
                 d2v_wd_matrix_gnn_kmeans = pairwise_wd_distance(concept_gnn_kmeans_attributes, nb_cc, model, words)
             d2v_wd_matrix_spectral_kmeans = pairwise_wd_distance(concept_spectral_kmeans_attributes, nb_cc, model, words)
             d2v_wd_matrix_d2v_kmeans = pairwise_wd_distance(concept_doc2vec_kmeans_attributes, nb_cc, model, words)
 
             # Save result
-            with open(f'{outpath}/wasserstein_distances_{dataset}_{b}_{s}_patterns.pkl', 'wb') as f:
+            with open(f'{outpath}/new/wasserstein_distances_{dataset}_{b}_{s}_patterns.pkl', 'wb') as f:
                 np.save(f, d2v_wd_matrix_concepts)
-            with open(f'{outpath}/wasserstein_distances_{dataset}_{b}_{s}_summaries.pkl', 'wb') as f:
+            with open(f'{outpath}/new/wasserstein_distances_{dataset}_{b}_{s}_summaries.pkl', 'wb') as f:
                 np.save(f, d2v_wd_matrix_summarized)
-            with open(f'{outpath}/wasserstein_distances_{dataset}_{b}_{s}_louvain.pkl', 'wb') as f:
-                np.save(f, d2v_wd_matrix_louvain)
+            if dataset != 'ingredients':
+                with open(f'{outpath}/new/wasserstein_distances_{dataset}_{b}_{s}_louvain.pkl', 'wb') as f:
+                    np.save(f, d2v_wd_matrix_louvain)
             if len(labels) > 0:
-                with open(f'{outpath}/wasserstein_distances_{dataset}_{b}_{s}_gnn_kmeans.pkl', 'wb') as f:
+                with open(f'{outpath}/new/wasserstein_distances_{dataset}_{b}_{s}_gnn_kmeans.pkl', 'wb') as f:
                     np.save(f, d2v_wd_matrix_gnn_kmeans)
-            with open(f'{outpath}/wasserstein_distances_{dataset}_{b}_{s}_spectral_kmeans.pkl', 'wb') as f:
+            with open(f'{outpath}/new/wasserstein_distances_{dataset}_{b}_{s}_spectral_kmeans.pkl', 'wb') as f:
                 np.save(f, d2v_wd_matrix_spectral_kmeans)
-            with open(f'{outpath}/wasserstein_distances_{dataset}_{b}_{s}_d2v_kmeans.pkl', 'wb') as f:
+            with open(f'{outpath}/new/wasserstein_distances_{dataset}_{b}_{s}_d2v_kmeans.pkl', 'wb') as f:
                 np.save(f, d2v_wd_matrix_d2v_kmeans)
