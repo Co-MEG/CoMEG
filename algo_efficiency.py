16a495c7627d50bfb4af26228f2df925f5ef876c
simondelarue <sim.delarue@gmail.com>
2023-03-13 15:10:04 +0100

    update attribute compressor

--- algo_efficiency.py
+++ algo_efficiency.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 from algorithm import run_unex_patterns
-from compressors import generation_complexity
+from compressors import generation_complexity, generation_complexity_attrs
 from data import load_data
 
 
@@ -14,16 +14,16 @@ from data import load_data
 # =================================================================
 
 #datasets = ['wikihumans']
-#datasets = ['wikivitals', 'wikivitals-fr', 'wikischools']
-datasets = ['wikischools']
+datasets = ['wikivitals', 'wikivitals-fr', 'wikischools']
+#datasets = ['wikischools']
 #datasets = ['ingredients']
 #datasets = ['sanFranciscoCrimes']
 #datasets = ['lastfm']
 
-betas = [5]
-ss = [5]
+betas = [4]
+ss = [8, 7, 6, 5]
 deltas = [0]
-OUTPATH = '/Users/simondelarue/Documents/PhD/Research/Co-Meg/CoMEG/output/result/with_prob'
+OUTPATH = '/Users/simondelarue/Documents/PhD/Research/Co-Meg/CoMEG/output/result/with_prob/attr_compressor'
 
 order_attributes = [True]
 
@@ -32,26 +32,34 @@ nb_pattern_dict = defaultdict(dict)
 
 for dataset in datasets:
     print(f'**** {dataset}')
+
     nb_pattern_dict[dataset] = defaultdict(dict)
+
+    # Load data
+    adjacency, biadjacency, names, words, labels = load_data(dataset)
+
     for order_attr in order_attributes:
         print(f'---- {order_attr}')
+
         nb_pattern_dict[dataset][order_attr] = defaultdict(list)
+
         for beta in betas:
             print(f'== {beta}')
+
             for s in ss:
                 print(f'- {s}')
+
                 for delt in deltas:
                             
                     outfile = f'{dataset}_{str(beta)}_{str(s)}_order{str(order_attr)}_delta_{delt}'
 
-                    # Load data
-                    adjacency, biadjacency, names, words, labels = load_data(dataset)
-
                     # Compute generation complexities
                     complexity_gen_graphs = generation_complexity(adjacency, biadjacency, n_attrs=15, n_iter=300)
-                    
+                    complexity_gen_attrs = generation_complexity_attrs(adjacency, biadjacency, n_attrs=15, n_iter=300)
+
                     # Run algorithm
-                    nb_patterns = run_unex_patterns(adjacency, biadjacency, words, complexity_gen_graphs, order_attr, s, beta, delt, outfile, OUTPATH)
+                    nb_patterns = run_unex_patterns(adjacency, biadjacency, words, complexity_gen_graphs,
+                                                    complexity_gen_attrs, order_attr, s, beta, delt, outfile, OUTPATH)
 
                     # Save number of patterns
                     nb_pattern_dict[dataset][order_attr][beta].append(nb_patterns)
